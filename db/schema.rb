1b81da8cf82ee77ab6a4ec8a2f0b0c2fee1e4e73
Roger Perez <rogperez52@gmail.com>
2018-01-27 17:21:07 -0800

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128010848) do
+ActiveRecord::Schema.define(version: 20180128011610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20180128010848) do
   create_table "configurables", force: :cascade do |t|
     t.string   "name"
     t.integer  "account_id"
+    t.jsonb    "configuration"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "creative_configurations", force: :cascade do |t|
+    t.integer  "plan_id"
     t.jsonb    "configuration"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
