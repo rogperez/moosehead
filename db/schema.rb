eca70e7e880e3cffbdffeb18dda7297b70c40fb2
Roger Perez <rogperez52@gmail.com>
2018-01-27 17:31:17 -0800

    create types

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128013023) do
+ActiveRecord::Schema.define(version: 20180128013051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 20180128013023) do
     t.string   "name"
     t.integer  "account_id"
     t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
